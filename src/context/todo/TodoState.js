356c4eab6334a39c976cac0d6ad7b8f0697122bf
Maxim <ivannikov.maxim1@gmail.com>
2024-06-18 14:22:13 +0200

    refactoring

--- src/context/todo/TodoState.js
+++ src/context/todo/TodoState.js
@@ -4,6 +4,7 @@ import { Alert } from 'react-native';
 import { todoReducer } from './todoReducer';
 import { ADD_TODO, CLEAR_ERROR, FETCH_TODOS, HIDE_LOADER, REMOVE_TODO, SHOW_ERROR, SHOW_LOADER, UPDATE_TODO } from '../types';
 import { ScreenContext } from '../screen/screenContext';
+import { Http } from '../../http';
 
 export const TodoState = ({ children }) => {
 	const initialState = {
@@ -13,18 +14,22 @@ export const TodoState = ({ children }) => {
 	};
 
 	const { changeScreen } = useContext(ScreenContext);
-
 	const [state, dispatch] = useReducer(todoReducer, initialState);
 
 	const addTodo = async (title) => {
-		const response = await fetch('https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos.json', {
-			method: 'POST',
-			headers: { 'Content-Type': 'application/json' },
-			body: JSON.stringify({ title }),
-		});
-		const data = await response.json();
-		// console.log('ID', data.name);
-		dispatch({ type: ADD_TODO, title, id: data.name });
+		// const response = await fetch('https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos.json', {
+		// 	method: 'POST',
+		// 	headers: { 'Content-Type': 'application/json' },
+		// 	body: JSON.stringify({ title }),
+		// });
+		// const data = await response.json();
+		clearError();
+		try {
+			const data = await Http.post('https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos.json', { title });
+			dispatch({ type: ADD_TODO, title, id: data.name });
+		} catch (error) {
+			showError('Something goes wrong ... ');
+		}
 	};
 	const removeTodo = (id) => {
 		const todo = state.todos.find((t) => t.id === id);
@@ -41,11 +46,11 @@ export const TodoState = ({ children }) => {
 					style: 'destructive',
 					onPress: async () => {
 						changeScreen(null);
-
-						await fetch(`https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos/${id}.json`, {
-							method: 'DELETE',
-							headers: { 'Content-Type': 'application/json' },
-						});
+						// await fetch(`https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos/${id}.json`, {
+						// 	method: 'DELETE',
+						// 	headers: { 'Content-Type': 'application/json' },
+						// });
+						await Http.delete(`https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos/${id}.json`);
 						dispatch({ type: REMOVE_TODO, id });
 					},
 				},
@@ -59,11 +64,12 @@ export const TodoState = ({ children }) => {
 		clearError();
 		showLoader();
 		try {
-			await fetch(`https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos/${id}.json`, {
-				method: 'PATCH',
-				headers: { 'Content-Type': 'application/json' },
-				body: JSON.stringify({ title }),
-			});
+			await Http.patch(`https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos/${id}.json`, { title });
+			// await fetch(`https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos/${id}.json`, {
+			// 	method: 'PATCH',
+			// 	headers: { 'Content-Type': 'application/json' },
+			// 	body: JSON.stringify({ title }),
+			// });
 			dispatch({ type: UPDATE_TODO, id, title });
 		} catch (e) {
 			showError('Something was wrong...');
@@ -82,11 +88,13 @@ export const TodoState = ({ children }) => {
 		clearError();
 
 		try {
-			const response = await fetch('https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos.json', {
-				method: 'GET',
-				headers: { 'Content-Type': 'application/json' },
-			});
-			const data = await response.json();
+			// const response = await fetch('https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos.json', {
+			// 	method: 'GET',
+			// 	headers: { 'Content-Type': 'application/json' },
+			// });
+			// const data = await response.json();
+
+			const data = await Http.get('https://rn-todo-app-9fa06-default-rtdb.europe-west1.firebasedatabase.app/todos.json');
 			const todos = Object.keys(data).map((key) => ({ ...data[key], id: key }));
 			dispatch({ type: FETCH_TODOS, todos });
 		} catch (e) {
